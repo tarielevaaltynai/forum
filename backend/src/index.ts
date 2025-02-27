0a92142b9f47c78f83d878fc8774027c6600f229
anarbaevaaidana2003 <aidanabakytkyzy2003@gmail.com>
2025-02-27 22:25:46 +0600

    FOR-7 <Splitting the tRPC backend into separate filese>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,21 @@
-import express from 'express'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { trpcRouter } from './trpc'
 import cors from 'cors'
+import express from 'express'
+import { applyTrpcToExpressApp } from './lib/trpc'
+import { trpcRouter } from './router'
 
 const expressApp = express()
 expressApp.use(cors())
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
 })
+applyTrpcToExpressApp(expressApp, trpcRouter)
 
 
-const expressApp = express()
-expressApp.get('/ping', (req, res) => {
-  res.send('pong')
+
+
+
+expressApp.listen(3000, () => {
+  console.info('Listening at http://localhost:3000')
 })
 expressApp.use(
   '/trpc',
