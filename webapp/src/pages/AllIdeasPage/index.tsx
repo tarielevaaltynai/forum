019d7997e600d39356645e5b44e6ac4a7c0013aa
aalli-innaa <a3372442@gmail.com>
2025-02-28 11:48:01 +0600

    Добавление scss

---
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+import { getViewIdeaRoute } from '../../lib/routes'
+import { trpc } from '../../lib/trpc'
+import css from './index.module.scss'
+
+export const AllIdeasPage = () => {
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
+
+  return (
+    <div>
+      <h1 className={css.title}>All Ideas</h1>
+      <div className={css.ideas}>
+        {data.ideas.map((idea) => (
+          <div className={css.idea} key={idea.nick}>
+            <h2 className={css.ideaName}>
+              <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                {idea.name}
+              </Link>
+            </h2>
+            <p className={css.ideaDescription}>{idea.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
