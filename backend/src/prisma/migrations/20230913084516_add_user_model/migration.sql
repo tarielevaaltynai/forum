22de96a45c96928285a5ed1296b327610019bf8e
anarbaevaaidana2003 <aidanabakytkyzy2003@gmail.com>
2025-02-28 08:35:22 +0600

    FOR-17 <end point of registrations>

---
+++ backend/src/prisma/migrations/20230913084516_add_user_model/migration.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "nick" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nick_key" ON "User"("nick");
