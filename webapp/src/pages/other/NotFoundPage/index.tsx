66e25e23b1a69cb158ff436c8585411b5421d0f0
aalli-innaa <a3372442@gmail.com>
2025-03-23 23:37:56 +0600

    Загрузка картинок

--- webapp/src/pages/other/NotFoundPage/index.tsx
+++ webapp/src/pages/other/NotFoundPage/index.tsx
@@ -1,9 +1,15 @@
-import { ErrorPageComponent } from '../../../components/ErrorPageComponent'
+import image404 from "../../../assets/images/404.png";
+import { ErrorPageComponent } from "../../../components/ErrorPageComponent";
+import css from "./index.module.scss";
 
 export const NotFoundPage = ({
-  title = 'Не найдено',
-  message = 'Эта страница не существует',
+  title = "Не найдено",
+  message = "Эта страница не существует",
 }: {
-  title?: string
-  message?: string
-}) => <ErrorPageComponent title={title} message={message} />
+  title?: string;
+  message?: string;
+}) => (
+  <ErrorPageComponent title={title} message={message}>
+    <img src={image404} className={css.image} alt="" width="800" height="600" />
+  </ErrorPageComponent>
+);
