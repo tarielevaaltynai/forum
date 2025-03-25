aa893d5c7b7c5f325117c14dae9331f5ab4ae179
aalli-innaa <a3372442@gmail.com>
2025-03-26 04:40:46 +0600

    e-mail во фронт

--- backend/src/router/auth/signUp/index.ts
+++ backend/src/router/auth/signUp/index.ts
@@ -1,30 +1,38 @@
-import { trpc } from '../../../lib/trpc'
-import { zSignUpTrpcInput } from './input'
-import { getPasswordHash } from '../../../utils/getPasswordHash'
-import { signJWT } from '../../../utils/signJWT'
+import { trpc } from "../../../lib/trpc";
+import { zSignUpTrpcInput } from "./input";
+import { getPasswordHash } from "../../../utils/getPasswordHash";
+import { signJWT } from "../../../utils/signJWT";
 
-export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(async ({ ctx, input }) => {
-  const exUser = await ctx.prisma.user.findUnique({
-    where: {
-      nick: input.nick,
-      
-    },
-  })
-  if (exUser) {
-    throw new Error('Пользователь с таким ником уже существует')
-  }
-  const user = await ctx.prisma.user.create({
-    data: {
-      nick: input.nick,
-      password: getPasswordHash(input.password),
-      name:input.name,
-      surname:input.surname,
-      birthDate:input.birthDate,
-      gender:input.gender
-
-
-    },
-  })
-  const token = signJWT(user.id)
-  return { token }
-})
+export const signUpTrpcRoute = trpc.procedure
+  .input(zSignUpTrpcInput)
+  .mutation(async ({ ctx, input }) => {
+    const exUserWithNick = await ctx.prisma.user.findUnique({
+      where: {
+        nick: input.nick,
+      },
+    });
+    if (exUserWithNick) {
+      throw new Error("Пользователь с таким ником уже существует");
+    }
+    const exUserWithEmail = await ctx.prisma.user.findUnique({
+      where: {
+        email: input.email,
+      },
+    });
+    if (exUserWithEmail) {
+      throw new Error("User with this email already exists");
+    }
+    const user = await ctx.prisma.user.create({
+      data: {
+        nick: input.nick,
+        email: input.email,
+        password: getPasswordHash(input.password),
+        name: input.name,
+        surname: input.surname,
+        birthDate: input.birthDate,
+        gender: input.gender,
+      },
+    });
+    const token = signJWT(user.id);
+    return { token };
+  });
