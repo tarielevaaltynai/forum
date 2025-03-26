5b5f43f55cf6d36c4de5c828cc15364c0aa0404b
anarbaevaaidana2003 <aidanabakytkyzy2003@gmail.com>
2025-03-26 14:04:57 +0600

    create optional env

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -2,17 +2,25 @@ import * as dotenv from 'dotenv'
 import { z } from 'zod'
 
 dotenv.config()
+const zNonemptyTrimmed = z.string().trim().min(1)
+const zNonemptyTrimmedRequiredOnNotLocal = zNonemptyTrimmed.optional().refine(
+  // eslint-disable-next-line node/no-process-env
+  (val) => process.env.HOST_ENV === 'local' || !!val,
+  'Required on local host'
+)
+
 
 const zEnv = z.object({
-  PORT: z.string().trim().min(1),
-  DATABASE_URL: z.string().trim().min(1),
-  JWT_SECRET: z.string().trim().min(1),
-  PASSWORD_SALT: z.string().trim().min(1),
-  INITIAL_ADMIN_PASSWORD: z.string().trim().min(1),
-  WEBAPP_URL: z.string().trim().min(1),
-  BREVO_API_KEY: z.string().trim().min(1),
-  FROM_EMAIL_NAME: z.string().trim().min(1),
-  FROM_EMAIL_ADDRESS: z.string().trim().min(1),
+  PORT: zNonemptyTrimmed,
+  HOST_ENV: z.enum(['local', 'production']),
+  DATABASE_URL: zNonemptyTrimmed,
+  JWT_SECRET: zNonemptyTrimmed,
+  PASSWORD_SALT: zNonemptyTrimmed,
+  INITIAL_ADMIN_PASSWORD: zNonemptyTrimmed,
+  WEBAPP_URL: zNonemptyTrimmed,
+  BREVO_API_KEY: zNonemptyTrimmedRequiredOnNotLocal,
+  FROM_EMAIL_NAME: zNonemptyTrimmed,
+  FROM_EMAIL_ADDRESS: zNonemptyTrimmed,
 })
 
 // eslint-disable-next-line node/no-process-env
