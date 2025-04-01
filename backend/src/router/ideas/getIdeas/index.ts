adb8e676a7d21f5c8a51cda0d0a2ef0eb035f1d0
aalli-innaa <a3372442@gmail.com>
2025-04-01 09:17:58 +0600

    Add commit before pull

--- backend/src/router/ideas/getIdeas/index.ts
+++ backend/src/router/ideas/getIdeas/index.ts
@@ -1,65 +1,68 @@
-
-import { trpc } from '../../../lib/trpc'
-import _ from 'lodash'
-import { zGetIdeasTrpcInput } from './input'
-export const getIdeasTrpcRoute = trpc.procedure.input(zGetIdeasTrpcInput).query(async ({ ctx, input }) => {
+import { trpc } from "../../../lib/trpc";
+import _ from "lodash";
+import { zGetIdeasTrpcInput } from "./input";
+export const getIdeasTrpcRoute = trpc.procedure
+  .input(zGetIdeasTrpcInput)
+  .query(async ({ ctx, input }) => {
     // const normalizedSearch = input.search ? input.search.trim().replace(/[\s\n\t]/g, '_') : undefined
-    const normalizedSearch = input.search ? input.search.trim().replace(/[\s\n\t]/g, ' & ') : undefined
-  const rawIdeas = await ctx.prisma.idea.findMany({
-    select: {
-      id: true,
-      nick: true,
-      name: true,
-      description: true,
-      createdAt:true,
-      serialNumber: true,
-      _count: {
-        select: {
-          ideasLikes: true,
+    const normalizedSearch = input.search
+      ? input.search.trim().replace(/[\s\n\t]/g, " & ")
+      : undefined;
+    const rawIdeas = await ctx.prisma.idea.findMany({
+      select: {
+        id: true,
+        nick: true,
+        name: true,
+        description: true,
+        createdAt: true,
+        serialNumber: true,
+        _count: {
+          select: {
+            ideasLikes: true,
+          },
         },
       },
-    },
-    where: {
-      blockedAt: null,
-      ...(!normalizedSearch
-        ? {}
-        : {
-            OR: [
-              {
-                name: {
-                  search: normalizedSearch,
+      where: {
+        blockedAt: null,
+        ...(!normalizedSearch
+          ? {}
+          : {
+              OR: [
+                {
+                  name: {
+                    search: normalizedSearch,
+                  },
                 },
-              },
-              {
-                description: {
-                  search: normalizedSearch,
+                {
+                  description: {
+                    search: normalizedSearch,
+                  },
                 },
-              },
-              {
-                text: {
-                  search: normalizedSearch,
+                {
+                  text: {
+                    search: normalizedSearch,
+                  },
                 },
-              },
-            ],
-          }),
-    },
-    orderBy: [
-      {
-        createdAt: 'desc',
+              ],
+            }),
       },
-      {
-        serialNumber: 'desc',
-      },
-    ],
-    cursor: input.cursor ? { serialNumber: input.cursor } : undefined,
-    take: input.limit + 1,
-  })
-  const nextIdea = rawIdeas.at(input.limit)
-  const nextCursor = nextIdea?.serialNumber
-  const rawIdeasExceptNext = rawIdeas.slice(0, input.limit)
-  const ideasExceptNext = rawIdeasExceptNext.map((idea) => ({
-    ..._.omit(idea, ['_count']),
-    likesCount: idea._count.ideasLikes,
-  }))
-  return { ideas: ideasExceptNext, nextCursor }
-})
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+        {
+          serialNumber: "desc",
+        },
+      ],
+      cursor: input.cursor ? { serialNumber: input.cursor } : undefined,
+      take: input.limit + 1,
+    });
+    const nextIdea = rawIdeas.at(input.limit);
+    const nextCursor = nextIdea?.serialNumber;
+    const rawIdeasExceptNext = rawIdeas.slice(0, input.limit);
+    const ideasExceptNext = rawIdeasExceptNext.map((idea) => ({
+      ..._.omit(idea, ["_count"]),
+      likesCount: idea._count.ideasLikes,
+    }));
+    return { ideas: ideasExceptNext, nextCursor };
+  });
--- backend/src/router/ideas/getIdeas/input.ts
+++ backend/src/router/ideas/getIdeas/input.ts
@@ -1,9 +1,8 @@
-import { z } from 'zod'
+import { z } from "zod";
 
-import { zStringOptional } from '@forum_project/shared/src/zod'
+import { zStringOptional } from "@forum_project/shared/src/zod";
 export const zGetIdeasTrpcInput = z.object({
   cursor: z.coerce.number().optional(),
   limit: z.number().min(1).max(100).default(10),
   search: zStringOptional,
-
-})
+});
