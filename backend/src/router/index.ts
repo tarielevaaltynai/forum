f4fb2cd1d438dcf3fd4a43eed3c6fc4d9f3d5ada
tarielevaaltynai <tarielevaaltynai@gmail.com>
2025-04-09 21:30:13 +0600

    profile

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,24 +1,23 @@
-import { createTrpcRouter } from "../lib/trpc";
+import { createTrpcRouter } from '../lib/trpc'
 
 // @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
-import { getMeTrpcRoute } from "./auth/getMe";
-import { signInTrpcRoute } from "./auth/signIn";
-import { signUpTrpcRoute } from "./auth/signUp";
-import { createIdeaTrpcRoute } from "./ideas/createIdea";
-import { getIdeaTrpcRoute } from "./ideas/getIdea";
-import { setIdeaLikeTrpcRoute } from "./ideas/setIdeaLIke";
-import { getIdeasTrpcRoute } from "./ideas/getIdeas";
-import { updateIdeaTrpcRoute } from "./ideas/updateIdea";
-import { updateProfileTrpcRoute } from "./auth/updateProfile";
-import { updatePasswordTrpcRoute } from "./auth/updatePassword";
-import { updateAvatarTrpcRoute } from "./auth/updateAvatar";
-import { prepareCloudinaryUploadTrpcRoute } from "./upload/prepareCloudinaryUpload";
-import { blockIdeaTrpcRoute } from "./ideas/blockIdea";
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-import { createCommentTrpcRoute } from "./ideas/createComment";
-import { getCommentsTrpcRoute } from "./ideas/getComments";
-import { getRepliesTrpcRoute } from "./ideas/getReplies";
-import { createReplyTrpcRoute } from "./ideas/createReply";
+import { getMeTrpcRoute } from './auth/getMe'
+import { signInTrpcRoute } from './auth/signIn'
+import { signUpTrpcRoute } from './auth/signUp'
+import { createIdeaTrpcRoute } from './ideas/createIdea'
+import { getIdeaTrpcRoute } from './ideas/getIdea'
+import { setIdeaLikeTrpcRoute } from './ideas/setIdeaLIke'
+import { getIdeasTrpcRoute } from './ideas/getIdeas'
+import { updateIdeaTrpcRoute } from './ideas/updateIdea'
+import { updateProfileTrpcRoute } from './auth/updateProfile'
+import { updatePasswordTrpcRoute } from './auth/updatePassword'
+import { updateAvatarTrpcRoute } from './auth/updateAvatar'
+import { prepareCloudinaryUploadTrpcRoute } from './upload/prepareCloudinaryUpload'
+import { blockIdeaTrpcRoute } from './ideas/blockIdea'
+import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
+import { createCommentTrpcRoute } from './ideas/createComment'
+import { getCommentsTrpcRoute } from './ideas/getComments'
+import { getRepliesTrpcRoute } from './ideas/getReplies'
 // @endindex
 export const trpcRouter = createTrpcRouter({
   // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
@@ -28,12 +27,12 @@ export const trpcRouter = createTrpcRouter({
   signUp: signUpTrpcRoute,
   updateProfile: updateProfileTrpcRoute,
   updateAvatar: updateAvatarTrpcRoute,
-  createComment: createCommentTrpcRoute,
-  getComments: getCommentsTrpcRoute,
-  getReplies: getRepliesTrpcRoute,
+  createComment:createCommentTrpcRoute,
+getComments:getCommentsTrpcRoute,
+getReplies:getRepliesTrpcRoute,
   blockIdea: blockIdeaTrpcRoute,
   updatePassword: updatePasswordTrpcRoute,
-  createReply: createReplyTrpcRoute,
+
   createIdea: createIdeaTrpcRoute,
   getIdea: getIdeaTrpcRoute,
   getIdeas: getIdeasTrpcRoute,
@@ -42,8 +41,10 @@ export const trpcRouter = createTrpcRouter({
   prepareCloudinaryUpload: prepareCloudinaryUploadTrpcRoute,
 
   // @endindex
-});
+})
 
-export type TrpcRouter = typeof trpcRouter;
-export type TrpcRouterInput = inferRouterInputs<TrpcRouter>;
-export type TrpcRouterOutput = inferRouterOutputs<TrpcRouter>;
+export type TrpcRouter = typeof trpcRouter
+export type TrpcRouterInput = inferRouterInputs<TrpcRouter>
+export type TrpcRouterOutput = inferRouterOutputs<TrpcRouter>
+
+
